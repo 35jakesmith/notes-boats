ea09c694708bc5e01504d260935aab32495194e9
35jakesmith <35jakesmith@gmail.com>
2022-03-15 16:58:45 -0600

    updated server and finalized routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use(require('./noteRoutes'));
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const path = require('path');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}...`);
-})
+    console.log(`WORKING ON PORT ${PORT}!`)
+});
